b71ada9f44c4625d68f76b4ccf0f9a4e418e9263
Alvaro <92274708+Ryuuse1@users.noreply.github.com>
2023-10-10 20:25:34 -0600

    cambios cs Menu Configuración

--- proyectoV5/menu/MenuConfiguracion.aspx.cs
+++ proyectoV5/menu/MenuConfiguracion.aspx.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
