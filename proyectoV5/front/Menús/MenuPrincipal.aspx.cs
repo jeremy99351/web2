6d0014db53d0e2aef332c94bbe962994cee26a73
Alvaro <92274708+Ryuuse1@users.noreply.github.com>
2023-10-24 16:29:03 -0600

    menus
    se agregan los menus

---
+++ proyectoV5/front/Menús/MenuPrincipal.aspx.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class front_MenuPrincipal : System.Web.UI.Page
+{
+    string cadenaConexion = System.Configuration.ConfigurationManager.ConnectionStrings["conexionCALUFA"].ConnectionString;
+}
